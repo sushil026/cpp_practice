1397dca6738b64fdf6f58d931406ec94d573cbac
sushil026 <68173862+sushil026@users.noreply.github.com>
2023-12-14 13:32:51 +0530

    reverse()

--- basic_maths.cpp
+++ basic_maths.cpp
@@ -12,9 +12,19 @@ void countDigits2( int num ){
     string str = to_string(num);
     cout << str.size();
 }
+void reverse(int x) {
+    long long int res = 0;
+    while( x){
+        res = (res*10)+ x%10;
+        x = x/10;
+    }
+    cout << int(res);
+}
 int main() {
     cout << "Basic maths!"<< endl;
     // countDigits(101);
     // countDigits2(101);
+    // reverse(1234);
+    
     return 0;
 }
