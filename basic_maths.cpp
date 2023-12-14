1bfc5aa4201fb1501d34bcc8b78f7ecfe79be48b
sushil026 <68173862+sushil026@users.noreply.github.com>
2023-12-14 12:54:13 +0530

    countDigits

--- basic_maths.cpp
+++ basic_maths.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+void countDigits( int num ){
+    int c = 0;
+    while( num ){
+        c++;
+        num = num/10;
+    }
+    cout << c;
+}
+int main() {
+    cout << "Basic maths!"<< endl;
+    // countDigits(101);
+    return 0;
+}
