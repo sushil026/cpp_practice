2173cd4c9f4ee3e23fb417426029cb6f9116ac75
sushil026 <68173862+sushil026@users.noreply.github.com>
2023-12-15 23:11:09 +0530

    printCountingTo1( )

--- basic_recursion.cpp
+++ basic_recursion.cpp
@@ -7,8 +7,16 @@ void printCounting( int x, int i){
     cout << i<< endl;
     printCounting( x, i+1);
 }
+void printCountingTo1( int x ){
+    if( x == 0 ){
+        return;
+    }
+    cout << x<< endl;
+    printCounting( x-1);
+}
 int main() {
     cout << "Basic recursion!"<< endl;
-    printCounting(5, 1);
+    // printCounting(5, 1);
+    // printCountingTo1(5);
     return 0;
 }
