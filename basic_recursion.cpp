00e51699504a42f4563f79a4639b708a01890b4f
sushil026 <68173862+sushil026@users.noreply.github.com>
2023-12-15 23:59:49 +0530

    factorial2(3);

--- basic_recursion.cpp
+++ basic_recursion.cpp
@@ -37,12 +37,20 @@ void factorial(int i, int fac){
     }
     factorial( i-1, fac*i);
 }
+// FUNCTIONAL METHOD
+int factorial2(int i){
+    if( i == 0){
+        return 1;
+    }
+    return i*factorial2( i-1 );
+}
 int main() {
     cout << "Basic recursion!"<< endl;
     // printCounting(5, 1);
     // printCountingTo1(5);
     // printSum(9, 0);
     // cout << printSum2(9);
-    factorial(3, 1);
+    // factorial(3, 1);
+    // cout << factorial2(3);
     return 0;
 }
