5f9ce6857895cdfbc71a9d39b491131be691407b
sushil026 <68173862+sushil026@users.noreply.github.com>
2023-12-18 00:49:57 +0530

    highestLowest()

--- basic_hashing.cpp
+++ basic_hashing.cpp
@@ -22,9 +22,35 @@ void frequencyOfLetters(){
     cout<< endl;
     cout<< freq_map['W']; // gives output as the value at key-value pair is available with key as 'W'
 }
+void highestLowest(){
+    map<int, int> freq_map;
+    vector<int> arr = {10,5,10,15,10,5};
+    for( auto el: arr){
+        cout<< el<< " ";
+        freq_map[el]++;
+    }
+    cout<< endl;
+    int n = arr.size();
+    int max_fr = 0;
+    int min_fr = n;
+    int min_el = 0;
+    int max_el = 0;
+    for( auto it: freq_map){
+        if( it.second > max_fr ){
+            max_fr = it.second;
+            max_el = it.first;
+        }
+        if( it.second < min_fr ){
+            min_fr = it.second;
+            min_el = it.first;
+        }
+    }
+    cout<< max_el<< " " << min_el;
+}
 int main() {
     cout << "Basic Hashing!"<< endl;
-    // frequencyOfElements()
-    // frequencyOfLetters()
+    // frequencyOfElements();
+    // frequencyOfLetters();
+    // highestLowest();
     return 0;
 }
