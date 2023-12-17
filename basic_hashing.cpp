d6afc4033359f1a11671a0e253353ef0deb357b1
sushil026 <68173862+sushil026@users.noreply.github.com>
2023-12-18 00:27:24 +0530

    frequencyOfElements()

--- basic_hashing.cpp
+++ basic_hashing.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+void frequencyOfElements(){
+    map<int, int> freq_map;
+    int arr[] = {10,5,10,15,10,5};
+    for( auto el: arr){
+        cout<< el<< " ";
+        freq_map[el]++;
+    }
+    cout<< endl;
+    for( auto el: freq_map){
+        cout<< el.first<< "-"<< el.second<< endl;
+    }
+    cout<< freq_map[20]; // gives output as 0 as no key-value pair is available with key as 20 
+}
 int main() {
     cout << "Basic Hashing!"<< endl;
+    // frequencyOfElements()
     return 0;
 }
