f3f9827be4e745041c55bd01ec34b1968184cc65
sushil026 <68173862+sushil026@users.noreply.github.com>
2023-12-18 00:32:17 +0530

    frequencyOfLetters()

--- basic_hashing.cpp
+++ basic_hashing.cpp
@@ -13,8 +13,18 @@ void frequencyOfElements(){
     }
     cout<< freq_map[20]; // gives output as 0 as no key-value pair is available with key as 20 
 }
+void frequencyOfLetters(){
+    map<char, int> freq_map;
+    string str = "Whispers of autumn leaves descend";
+    for( auto el: str){
+        freq_map[el]++;
+    }
+    cout<< endl;
+    cout<< freq_map['W']; // gives output as the value at key-value pair is available with key as 'W'
+}
 int main() {
     cout << "Basic Hashing!"<< endl;
     // frequencyOfElements()
+    // frequencyOfLetters()
     return 0;
 }
