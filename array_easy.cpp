28ab749aa0f09b5fefb28631aff56c9d21d80c59
sushil026 <68173862+sushil026@users.noreply.github.com>
2023-12-20 19:18:42 +0530

    removeDupes2()

--- array_easy.cpp
+++ array_easy.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// utilises extra space to store distinct elements
 void removeDupes(){
     vector<int> res;
     vector<int> arr = {1, 2, 3, 3, 3, 3};
@@ -14,7 +16,24 @@ void removeDupes(){
         cout << el << " ";
     }
 }
+// manipulates the available array to store distinct elements at beginning
+void removeDupes2(){
+    vector<int> res;
+    vector<int> arr = {1, 2, 3, 3, 3, 3 };
+    int ind = 0;
+      for (int i = 0; i < arr.size(); i++) {
+        int k = arr[i];
+        arr[ind++] = k;
+        while (i + 1 < arr.size() && arr[i + 1] == k) {
+            i++;
+        }
+    }
+    for( int i = 0; i< ind; i++){
+        cout << arr[i] << " ";
+    }
+}
 int main() {
-    removeDupes();
+    // removeDupes();
+    // removeDupes2()
     return 0;
 }
