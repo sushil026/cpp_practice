97a43de9fc756748b44746df525062af9997b20b
sushil026 <68173862+sushil026@users.noreply.github.com>
2023-12-20 19:10:31 +0530

    removeDuplicates()

--- array_easy.cpp
+++ array_easy.cpp
@@ -1,1 +1,20 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+void removeDupes(){
+    vector<int> res;
+    vector<int> arr = {1, 2, 3, 3, 3, 3};
+      for (int i = 0; i < arr.size(); i++) {
+        int k = arr[i];
+        res.push_back(k);  
+        while (i + 1 < arr.size() && arr[i + 1] == k) {
+            i++;
+        }
+    }
+    for( int el: res){
+        cout << el << " ";
+    }
+}
+int main() {
+    removeDupes();
+    return 0;
+}
